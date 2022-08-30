0f0592628a56ae09575d3882bc298028360e0ecd
Raji Omogbolahan Al-ameen <alameenraji1@gmail.com>
2022-08-30 12:56:40 +0100

    fizzbuzz challenge

--- index.js
+++ index.js
@@ -247,4 +247,21 @@ function result(num) {
     alert("Sorry, you are not allowed entry!");
   }
   
+  // FizzBuzz using if..else and for statement 
   
+  let output = [];
+  function fizzBuzz() {
+    for (let i = 1; i < 101; i++)
+    if (i % 3 === 0 && i % 5 === 0) {
+      output.push("fizzBuzz")
+    } else if (i % 3 === 0) {
+      output.push("fizz")
+    } else if (i % 5 === 0) {
+      output.push("buzz")
+    } else {
+      output.push(i);
+    }
+    console.log(output)
+  }
+  
+  fizzBuzz()
