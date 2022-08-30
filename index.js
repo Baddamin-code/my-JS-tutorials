aaa1356b6b99e46db84d5d6c11fa29022bae481c
Raji Omogbolahan Al-ameen <alameenraji1@gmail.com>
2022-08-30 12:22:04 +0100

    concatenation and console logging

--- index.js
+++ index.js
@@ -8,3 +8,11 @@ a = "I am ";
 b = "an aspiring";
 c = " web developer";
 
+//concatenation & console logging
+console.log(a+b+c)
+
+let firstName = "Al-ameen";
+let lastName = "Raji";
+let myName = firstName + " " + lastName;
+console.log(myName)
+
